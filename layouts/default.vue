3809fe708fcf8dc61c41bcb3a4ba0c46c7f71b62
sebastian200 <kotte.alin@gmail.com>
2024-01-09 15:46:56 +0100

    added folders for compnents and immages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,21 @@
 <template>
   <header class="flex justify-between p-1 pr-2 pl-2 h-16 border-solid border-gray-200 border-b-2">
+    <div class="flex w-screen ">
+      <div>      
+        <img class="h-10" src="../assets/Images/—Pngtree—books logo_4135439.png" alt="">
+        <nuxt-link to="">Booking</nuxt-link></div>
+      <div class="flex justify-center">
+        
+        <nuxt-link to="../pages/index.vue">Home</nuxt-link>
+        <nuxt-link to="">Suggestions</nuxt-link>
+      </div>
+    </div>
+
   </header>
+ 
   <main class="h-screen">
+
+    
     <slot />
   </main>
   <footer class="">
