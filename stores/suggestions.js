3c210dc5a0f145fc7a84a55b2023b520e96547db
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2024-01-18 18:03:28 +0100

    Modified the classes and the suggestions store (changed toJSON and fromJSON).

--- stores/suggestions.js
+++ stores/suggestions.js
@@ -9,26 +9,33 @@ export const useSuggestionsStore = defineStore("suggestions", {
   }),
   getters: {
     getSuggestions() {
-      return this.suggestions
+      return this.suggestions.map(suggestion => Suggestion.fromJSON(suggestion))
     },
     getSuggestion() {
 
     }
   },
   actions: {
-    addSuggestion() {
-
+    addSuggestion(suggestion) {
+      // For some reason, suggestions can only contain POJOs
+      this.suggestions.push(suggestion.toJSON())
     },
     removeSuggestion() {
 
     },
     toJSON() {
       return {
-
+        suggestions: this.suggestions.map(suggestion => suggestion)
       }
     },
     fromJSON(json) {
-      
+      try {
+        json.suggestions.map(suggestion => this.suggestions.push(suggestion))
+      }
+      catch(error) {
+        console.log("Could not deserialize json to suggestions")
+        // console.log(error)
+      }
     }
   }
 })
