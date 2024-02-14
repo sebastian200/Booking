e48af50f0cca02ec83ce9f8d8aef898896c8312b
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2024-02-14 14:16:30 +0100

    Improved the createHash function. It creates somewhat unique hashes for every book.

--- classes/Book.js
+++ classes/Book.js
@@ -1,3 +1,5 @@
+const hashLength = 20
+
 /*
  * Params
  * - title  | the title of the book
@@ -7,9 +9,29 @@
  * - hash | the created book hash
  */
 const createHash = (title, author) => {
-  let hash = (title + author)
+  if(!title && !author) return null
 
-  return hash
+  let hash = new Array(hashLength).fill(0)
+
+  const start = (title ?? "") + (author ?? "")
+
+  for(let index in start) {
+    hash[index % hashLength] += start.charCodeAt(index)
+  }
+
+  // Making the hash look more random
+  for(let count = 0; count < 20; count++) {
+    for(let index = 0; index < hash.length - 1; index++) {
+      hash[index] += hash[index + 1]
+    }
+  }
+
+  // Clamp the numbers to ASCII codes
+  for(let index in hash) {
+    hash[index] = hash[index] % (122 - 97) + 97
+  }
+
+  return String.fromCharCode(...hash)
 }
 
 export default class Book {
